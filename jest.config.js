11b0784f9b550e5fd970e76ffb5660628a8231bf
Wouter Lindenhof <wouterlindenhof@gmail.com>
2021-02-16 22:15:59 +0100

    test script looks only in src folder and validates coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testRegex: '/src/.+\.test\.ts$',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    }
+  }
 };
