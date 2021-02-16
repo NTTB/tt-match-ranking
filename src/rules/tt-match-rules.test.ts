c491fadc36490f4f7e38cbb67a7b2c9127f425c9
Wouter Lindenhof <wouterlindenhof@gmail.com>
2021-02-16 23:04:34 +0100

    Add match-rules parsing

--- src/rules/tt-match-rules.test.ts
+++ src/rules/tt-match-rules.test.ts
@@ -1,13 +1,47 @@
-import { assertMatchRules } from "./tt-match-rules";
+import { assertMatchRules, parseMatchRules } from "./tt-match-rules";
+
+describe.each`
+  winning | losing | input
+  ${1}    | ${0}   | ${"+1/0"}
+  ${2}    | ${0}   | ${"+2/0"}
+  ${2}    | ${1}   | ${"+2/1"}
+`("parseMatchRules(\"$input\")", ({ winning, losing, input }) => {
+  const rules = parseMatchRules(input);
+  test(`should set victoryPoints to ${winning}`, () => expect(rules.victoryPoints).toBe(winning));
+  test(`should set defeatPoints to ${losing}`, () => expect(rules.defeatPoints).toBe(losing));
+});
+
+describe("parseMatchRules input validation", () => {
+  test("parseMatchRules(\"0/0\") should throw", () => {
+    const act = () => parseMatchRules("0/0");
+    expect(act).toThrowError(/Unable to parse match rules from/);
+  });
+
+  test("parseMatchRules(null) should throw", () => {
+    const act = () => parseMatchRules(null as any);
+    expect(act).toThrowError(/Unable to parse match rules from/);
+  });
+
+  test("parseMatchRules(undefined) should throw", () => {
+    const act = () => parseMatchRules(undefined as any);
+    expect(act).toThrowError(/Unable to parse match rules from/);
+  });
+
+  test("parseMatchRules(\"invalidString\") should throw", () => {
+    const act = () => parseMatchRules("invalidString");
+    expect(act).toThrowError(/Unable to parse match rules from/);
+  });
+});
 
 describe("assertMatchRules(...)", () => {
   test("should throw when lose score is higher equal to win score", () => {
-    const act = () => assertMatchRules({ pointsPerWin: 0, pointsPerLose: 0 });
-    expect(act).toThrowError(/The pointsPerWin should be higher than the pointsPerLose/);
+    const act = () => assertMatchRules({ victoryPoints: 0, defeatPoints: 0 });
+    expect(act).toThrowError(/The victoryPoints should be higher than the defeatPoints/);
   });
+
   test("should throw when lose score is negative", () => {
-    const act = () => assertMatchRules({ pointsPerWin: 0, pointsPerLose: -1 });
-    expect(act).toThrowError(/The pointsPerLose must be zero or positive/);
+    const act = () => assertMatchRules({ victoryPoints: 0, defeatPoints: -1 });
+    expect(act).toThrowError(/The defeatPoints must be zero or positive/);
   });
 
   test.each`
@@ -15,7 +49,7 @@ describe("assertMatchRules(...)", () => {
     ${2}    | ${1}
     ${1}    | ${0}
   `("should not throw when a valid configuration is provided", ({ winning, losing }) => {
-    const act = () => assertMatchRules({ pointsPerWin: winning, pointsPerLose: losing });
+    const act = () => assertMatchRules({ victoryPoints: winning, defeatPoints: losing });
     expect(act).not.toThrow();
   });
 });
--- src/rules/tt-match-rules.ts
+++ src/rules/tt-match-rules.ts
@@ -2,15 +2,27 @@ export interface TTMatchRules {
   /**
    * Points gained by winning.
    */
-  pointsPerWin: number;
+  victoryPoints: number;
 
   /**
    * Points gained by losing
    */
-  pointsPerLose: number;
+  defeatPoints: number;
 }
 
 export function assertMatchRules(rules: TTMatchRules) {
-  if (rules.pointsPerWin <= rules.pointsPerLose) throw new Error("The pointsPerWin should be higher than the pointsPerLose");
-  if (rules.pointsPerLose < 0) throw new Error("The pointsPerLose must be zero or positive");
+  if (rules.victoryPoints <= rules.defeatPoints) throw new Error("The victoryPoints should be higher than the defeatPoints");
+  if (rules.defeatPoints < 0) throw new Error("The defeatPoints must be zero or positive");
+}
+
+export function parseMatchRules(text: string): TTMatchRules {
+  const parsed = /\+(?<victoryPoints>\d+)\/(?<defeatPoints>\d+)/.exec(text);
+  if (!parsed || !parsed.groups) {
+    throw new Error("Unable to parse match rules from " + text);
+  }
+
+  return {
+    victoryPoints: parseInt(parsed.groups["victoryPoints"]),
+    defeatPoints: parseInt(parsed.groups["defeatPoints"])
+  };
 }
