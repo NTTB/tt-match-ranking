864945eee02908f82700314216fd4dd26ccdc0a7
Wouter Lindenhof <wouterlindenhof@gmail.com>
2021-02-20 22:55:01 +0100

    Add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
 };
